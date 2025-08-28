a00fb24e289a4190bb2c0ecb992326c49e7b698b
kylexrose@gmail.com <kylexrose@gmail.com>
2025-08-28 14:47:08 -0400

    mongodb atlas

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+require('dotenv').config({quiet: true})
 const mongoose = require('mongoose')
 const app = require('./app')
 
-mongoose.connect('mongodb://127.0.0.1:27017/todo-backend')
+mongoose.connect(process.env.MONGO_ADDRESS)
 .then(()=>{
     app.listen(3000, ()=>{
         console.log("MONGODB CONNECTED.")
